1e8f5ea07d21240b92d81b38824cb416dd8f8aa3
alexbokii <vanilinka33@gmail.com>
2014-09-27 14:15:40 +0100

    Reorganized script files

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
   //1. Bang effect
   $(".page-keeper, .unordered-line").hide();
   $(".unordered-books button").on('click', function() {
@@ -19,8 +18,19 @@ $(document).ready(function() {
     }
   });
 
+  //2. Height of h5 in ordered list
+  // Every li has own extra height for title. Count this height and the height of title here.
+  function setHeight() {
+    $(".ordered-books li").each(function() {
+      var h5 = $(this).find("h5")[0];
+      var img = $(this).find("img")[0];
 
-  
-  
+      var h5Height = $(h5).height();
+      var imgHeight = $(img).height();
 
+      var result = 250 - imgHeight - 24 - 20;
+      $(h5).height(result);
+    });
+  } 
+  
 }); // end of ready
