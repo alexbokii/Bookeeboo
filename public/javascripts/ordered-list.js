9f51f0bc1ef6e7ee481e195829a7475039120000
alexbokii <vanilinka33@gmail.com>
2014-10-05 13:37:00 +0100

    Added new buttons for delete signes for search field and for books in lists

--- public/javascripts/ordered-list.js
+++ public/javascripts/ordered-list.js
@@ -11,7 +11,7 @@
       $(".ordered-books ul").sortable(sortingOrderedBooks);
 
       $(".ordered-books").on({
-        mouseenter: function() {$(this).append("<div class='sorting-delete'>x</div>");},
+        mouseenter: function() {$(this).append("<div class='sorting-delete'></div>");},
         mouseleave: function() {$(this).find('.sorting-delete').remove();}
       }, "li");
 
@@ -144,7 +144,7 @@
     currentBook.currentPage = result; //send new value to local current object
     $.post('/api/books/page', {bookId: currentBook.id, page: result}, function() {
       console.log(result);
-      getQueue(rewriteOrderOfOrderedBooks);
+      getQueue(redrawOrderedListInHtml);
     });
   }
 
