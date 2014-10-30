6b800ab27fd4cf48d452bc0a94d3d87fd2dbd41d
ivanbokii <bokiyis@gmail.com>
2014-10-30 18:19:29 -0400

    changes due to the deploy

--- data/index.js
+++ data/index.js
@@ -3,7 +3,7 @@ var knex = require('knex')({
   connection: {
     host: '127.0.0.1',
     user: process.env.BOOKEEBOO_PG_USER,
-    password: '',
+    password: process.env.BOOKEEBOO_PG_PASSWORD,
     database: 'bookeeboo'
   }
 });
